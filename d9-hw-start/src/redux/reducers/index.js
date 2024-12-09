957cc857e7f1d3112c7fc6d31c5f29429d31f753
Michele Mandanici <michimandanici@tim.it>
2024-12-09 15:00:31 +0100

    initial commit

---
+++ d9-hw-start/src/redux/reducers/index.js
@@ -0,0 +1,14 @@
+const initialState = {
+  main: {
+    count: 0,
+  },
+}
+
+const mainReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+export default mainReducer
